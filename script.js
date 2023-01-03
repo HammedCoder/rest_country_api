17018f3f12e461e618f77caf1bd1d86aa2cd9410
HammedCoder <rafiuhammed718@gmail.com>
2023-01-03 19:00:26 +0100

    implementing search by country name

--- script.js
+++ script.js
@@ -1,82 +1,120 @@
 "use strict";
 
+let searchInput = document.querySelector(".search__country--input");
+const filterByRegion = document.getElementById("filter");
 const col = document.querySelector(".col-md");
 const row = document.querySelector(".cap");
 
 let message;
-const countryApi = async () => {
-  const resApi = await fetch("https://restcountries.com/v3.1/all");
-  const resData = await resApi.json();
-  // console.log(resData);
-  try {
-    resData.forEach((el) => {
-      message = `
-        <div class="col lists">
-          <div class="card shadow-sm" id="paginated-list">
-            <img src="${el.flags.png}" alt="${el.name.common}" class="img-fluid" title="${el.name.common} flag" style="height: 200px" />
+let resData = [];
 
-            <div class="card-body">
+if ("speechSynthesis" in window) {
+  const msg = new SpeechSynthesisUtterance();
+  msg.text = `Welcome to my Rest Country API application`;
+  // window.speechSynthesis.speak(msg);
+} else {
+  alert("Your browser did not support speech to text");
+}
 
-              <p class="card-title" style="font-weight:700">Country: ${el.name.official}</p>
-              <p class="card-text">Capital City: ${el.capital}</p>
-              <p class="card-text">Continent: ${el.region}</p>
-              <p class="card-text"> Population: ${el.population}</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-muted">9 mins</small>
+const displayCountry = (characters) => {
+  const htmlString = characters
+    .map((el) => {
+      return `
+      
+      <div class="col lists">
+        <div class="card shadow-sm overflow-hidden" id="paginated-list"  data-country-name='${
+          el.cca2
+        }'>
+        
+  
+          <img src="${el.flags.png}" alt="${
+        el.name.common
+      }" class="img-fluid" title="${
+        el.name.common
+      } flag" style="height: 200px" />
+
+          <div class="card-body">
+
+            <p class="card-title" style="font-weight:700">Country: ${
+              el.name.official
+            }</p>
+            <p class="card-text">Capital City: ${el.capital}</p>
+            <p class="card-text">Continent: ${el.region}</p>
+            <p class="card-text"> Population: ${el.population.toLocaleString()}</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
               </div>
+              <small class="text-muted">9 mins</small>
             </div>
           </div>
-        </div>`;
+        </div>
+      </div>`;
+    })
+    .join("");
+  row.innerHTML = htmlString;
+};
 
-      row.insertAdjacentHTML("beforeend", message);
-    });
+const countryApi = async () => {
+  try {
+    const resApi = await fetch("https://restcountries.com/v3.1/all");
+    resData = await resApi.json();
+    // console.log(resData);
+    displayCountry(resData);
   } catch (error) {
-    console.log(error);
+    console.error(error, "No connection");
   }
 };
+
+searchInput.addEventListener("keyup", (e) => {
+  let value = e.target.value.toLowerCase();
+  // console.log(value);
+  const filteredCountry = resData.filter((country) => {
+    // console.log(country.name.official.includes(value));
+    if (country) {
+      return country.name.official.toLowerCase().includes(value);
+    } else {
+      return "No data return";
+    }
+  });
+  // create a function to search the country object
+  displayCountry(filteredCountry);
+});
+
 countryApi();
 
+// const displayCountry = (country) => {
+//   country.forEach((el) => {
+//     message = `
+//       <div class="col lists">
+//         <div class="card shadow-sm overflow-hidden" id="paginated-list"  data-country-name='${
+//           el.cca2
+//         }'>
 
+//           <img src="${el.flags.png}" alt="${
+//       el.name.common
+//     }" class="img-fluid" title="${el.name.common} flag" style="height: 200px" />
 
-const btnSearch = document.querySelector("#button-search");
-let input = document.querySelector("#inputsearch");
-// const searchCountry = () => {
-//   // console.log(e);
-//   input = input.toLowerCase();
-//   let list = document.querySelector(".lists");
-//   for (let i = 0; i < list.length; i++) {
-//     const el = list[i];
-//     if (!el.toLowerCase().includes(input)) {
-//       el.style.display = "none";
-//     } else {
-//       el.style.display = list;
-//     }
-//   }
+//           <div class="card-body">
+
+//             <p class="card-title" style="font-weight:700">Country: ${
+//               el.name.official
+//             }</p>
+//             <p class="card-text">Capital City: ${el.capital}</p>
+//             <p class="card-text">Continent: ${el.region}</p>
+//             <p class="card-text"> Population: ${el.population.toLocaleString()}</p>
+//             <div class="d-flex justify-content-between align-items-center">
+//               <div class="btn-group">
+//                 <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+//                 <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+//               </div>
+//               <small class="text-muted">9 mins</small>
+//             </div>
+//           </div>
+//         </div>
+//       </div>`;
+
+//     row.insertAdjacentHTML("beforeend", message);
+//   });
 // };
-// btnSearch.addEventListener("click", searchCountry);
-
-const setResult = (result) => {
-  for (const country of result) {
-    // const item = country;
-  }
-};
-
-input.addEventListener("input", (e) => {
-  let value = e.target.value;
-  if (value && value.trim().length > 0) {
-    value = value.trim().toLowerCase();
-    console.log(value);
-    // create a function to search the country objects
-    setResult(
-      message.filter((country) => {
-        return country.name.common.includes(value);
-      })
-    );
-  } else {
-    // return not found
-  }
-});
